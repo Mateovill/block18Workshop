87155500bfe53fb00ab14d561f8b4c2b0412352f
Mateo Villezca-Rivera <mateojvr@Mateos-iMac.local>
2024-03-12 22:13:19 -0600

    final commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//Unit tests
+// 1. expect two intengers that are passed to be multiplied
+// expect anything other than an intenger that is passed in to be an error
+//expect a string that is passed to be an error
+//2. 
+// contactOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1])
+//expect the arrays to become one array
+//expect all odds numbers to be filtered out into a new array
+//expect all even numbers to remian in the same array
+
+//Functional Tests
+//1. When the user initially sets the cart need the check out station, the station should prompt them to sign in or create an account
+//2. if the user has an account, they can enter a phone number or email or scan a membership card 
+//3. if the user is not a member they should be prompted to enter an email or phone number, or choose a card by a near by stand to scan
+//4. once the user is logged in they should be able to beging the process but pressing a initalizing button
+//5. If the cart is empty, the kisok will alert the user that there is nothing in the cart
+//6. if there are items within the cart the kisosk will keep a tally of the items by name, price and quantatiy
+//7. once the items are accounted for there will be a prompt to verify if everything is correct 
+//8. if not the user will then be able to edit the cart in order to accuraretly reflect the items they have
+//9. if the information is correct, the user will be prompted with the payment method of either tap pay or credit/debit
+//10. the station will then print the reciept
